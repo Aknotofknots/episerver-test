94ac668c2755c5560735b9b2f0c8add8f6f7b3b7
Aknotofknots <christianahlsen@gmail.com>
2017-04-28 18:59:42 +0200

    initial commit

---
+++ Controllers/StartPageController.cs
@@ -0,0 +1,20 @@
+﻿using EPiServer.Web.Mvc;
+using EPIserverTestSite.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPIserverTestSite.Controllers
+{
+    // this controller is created to manage the start page
+    public class StartPageController : PageController<StartPage>
+    {
+        // GET: StartPage
+        public ActionResult Index(StartPage currentPage) // the currentPage argument binds the controller and the startpage together
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Pages/StartPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core; // use this namespace to be able to use the superclass "PageData" below
+using EPiServer.DataAnnotations; // to be able to specify the page type below
+
+namespace EPIserverTestSite.Models.Pages
+{
+    [ContentType] // this is the page type that we are creating. this is needed.
+
+    public class StartPage : PageData // derive from this to get be able to start a page
+    {
+
+    }
+}
